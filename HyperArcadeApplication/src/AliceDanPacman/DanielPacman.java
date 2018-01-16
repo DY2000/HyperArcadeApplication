0f03d94d206a92780970387c3344c13759599a12
BT_1N3_32 <BT_1N3_32@BT_1N3_32.bktech.local>
2018-01-16 15:20:36 -0500

    made pacman

--- HyperArcadeApplication/src/AliceDanPacman/DanielPacman.java
+++ HyperArcadeApplication/src/AliceDanPacman/DanielPacman.java
@@ -1,5 +1,9 @@
 package AliceDanPacman;
 
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+
 import guiTeacher.components.AnimatedComponent;
 import markGalaga.MarkPlayerMovement;
 
@@ -7,6 +11,10 @@ public class DanielPacman extends AnimatedComponent implements MarkPlayerMovemen
 
 	public DanielPacman(int x, int y, int w, int h) {
 		super(x, y, w, h);
-		// TODO Auto-generated constructor stub
+		 
+	}
+	public void drawImage(Graphics2D g) {
+		 g.setColor(Color.yellow);
+		  g.fillArc(0,0,100,100,20,270);
 	}
 }
