5150934ba5eff3885be034a2655203a5aeeda78c
BT_1N3_33 <BT_1N3_33@BT_1N3_33.bktech.local>
2018-01-16 14:35:47 -0500

    Update

--- HyperArcadeApplication/src/HyperArcade/DanielPacDot.java
+++ HyperArcadeApplication/src/HyperArcade/DanielPacDot.java
@@ -1,9 +1,8 @@
 package HyperArcade;
-
+import guiTeacher.components.AnimatedComponent;
 public class DanielPacDot extends DanielPickup {
+		public DanielPacDot(int x, int y, int w, int h) {
+			super(x, y, w, h);
+		}
 
-	public DanielPacDot() {
-		// TODO Auto-generated constructor stub
 	}
-
-}
--- HyperArcadeApplication/src/HyperArcade/DanielPickup.java
+++ HyperArcadeApplication/src/HyperArcade/DanielPickup.java
@@ -6,6 +6,10 @@ public class DanielPickup {
 		// TODO Auto-generated constructor stub
 	}
 
+	public DanielPickup(int x, int y, int w, int h) {
+		// TODO Auto-generated constructor stub
+	}
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
