f360c971a937802e4f5e2ecb1138bffe03c995b4
BT_1N3_33 <BT_1N3_33@BT_1N3_33.bktech.local>
2018-01-19 14:28:59 -0500

    qweqwe

--- HyperArcadeApplication/src/AliceDanPacman/DanielPacman.java
+++ HyperArcadeApplication/src/AliceDanPacman/DanielPacman.java
@@ -7,7 +7,7 @@ import java.awt.image.BufferedImage;
 import guiTeacher.components.AnimatedComponent;
 import markGalaga.MarkPlayerMovement;
 
-public class DanielPacman extends AnimatedComponent implements MarkPlayerMovemen, AliceMethods t{
+public class DanielPacman extends AnimatedComponent implements MarkPlayerMovement, AliceMethods {
 
 	public DanielPacman(int x, int y, int w, int h) {
 		super(x, y, w, h);
