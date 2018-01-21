59f079424c161a624b8a0e53e97a3831e756a141
Mark Parks <31749006+Mark-Parks@users.noreply.github.com>
2018-01-21 14:14:37 -0500

    Update MarkPlayerMovement.java

--- HyperArcadeApplication/src/markGalaga/MarkPlayerMovement.java
+++ HyperArcadeApplication/src/markGalaga/MarkPlayerMovement.java
@@ -1,10 +1,8 @@
 package markGalaga;
-import java.awt.event.KeyEvent;
 
 import guiTeacher.components.AnimatedComponent;
-import guiTeacher.interfaces.KeyedComponent;
 
-public abstract class MarkPlayerMovement extends AnimatedComponent implements KeyedComponent{
+public abstract class MarkPlayerMovement extends AnimatedComponent{
 
 	public MarkPlayerMovement(int x, int y, int w, int h) {
 		super(x, y, w, h);
@@ -12,17 +10,5 @@ public abstract class MarkPlayerMovement extends AnimatedComponent implements Ke
 	}
 
 	@Override
-	public abstract void keyPressed(KeyEvent arg0);
-
-	@Override
-	public abstract void keyReleased(KeyEvent e);
-
-	@Override
-	public abstract void keyTyped(KeyEvent e);
-
-	@Override
-	public abstract boolean isHovered(int x, int y);
-
-	@Override
-	public abstract void setFocus(boolean b);
+	public abstract void checkBehaviors();
 }
