f5512f9574cf551390809a9981f726022a977280
wwu5040 <31863372+wwu5040@users.noreply.github.com>
2018-01-30 11:11:11 -0500

    Update TetrisMain.java

--- HyperArcadeApplication/src/willTetris/TetrisMain.java
+++ HyperArcadeApplication/src/willTetris/TetrisMain.java
@@ -15,40 +15,56 @@ import guiTeacher.userInterfaces.FullFunctionScreen;
 
 public class TetrisMain extends FullFunctionScreen {
 
-	private boolean[][] board;
-	private Color[][] boardColors;
+	private Block[][] board;
 	private Color emptyColor;
 	private Block b;
+	private ArrayList<Block> Tetramino = = new ArrayList<Block>(4);
+	private ArrayList<Arraylist<Block>> Tetraminos = new ArrayList<ArrayList<<Block>>(7);
 
 	public TetrisMain(int width, int height) {
 		super(width, height);
 		emptyColor = Color.blue;
 		boardColors = new Color[10][20];
-		for (int w = 0; w < boardColors.length; w++) {
-			for (int h = 0; h < boardColors[w].length; h++) {
-				boardColors[w][h] = emptyColor;
-			}
-		}
+		//I PIECE
+		Tetramino.add(Block(3,0));
+		Tetramino.add(Block(4,0));
+		Tetramino.add(Block(5,0));
+		Tetramino.add(Block(6,0));
+		Tetraminos.add(Tetramino);
+		Tetramino = ArrayList<Block>() 
 	}
 
 	@Override
 	public void initAllObjects(List<Visible> viewObjects) {
-		b = new Block(100, 100, emptyColor);
-		b.setVisible(true);
-		viewObjects.add(b);
+		
+	}
+	
+	public void dropDown(){
+		for (Block b: Tetramino){
+			boolean canDropDown = true;
+			if(board[b.getX][b.getY - 1] != null) boolean canDropDown = false;
+		} else{
+			Tetramino.remove(0,5);
+			newPiece();
+		}
+		
+		if(canDropDown){
+			for(Block b: Tetramino){
+				b.setX(b.getX - 1);
+				b.setY(b.getY - 1);
+			}
+		}
+	}
+	
+	public void newPiece(){
+	
 	}
 
 	public void paint(Graphics g) {
-		
-		for (int w = 0; w < boardColors.length; w++) {
-			for (int h = 0; h < boardColors[w].length; h++) {
-				boardColors[w][h] = emptyColor;
-			}
-		}
-		
-		for (int w = 0; w < boardColors.length; w++) {
-			for (int h = 0; h < boardColors[w].length; h++) {
-				g.setColor(boardColors[w][h]);
+		for (int w = 0; w < board.length; w++) {
+			for (int h = 0; h < board[w].length; h++) {
+				g.setColor(Color.blue);
+				if(board[w][h] != null) g.setColor(boardColors[w][h]);
 				g.fillRect(w * 30, h * 30, 25, 25);
 			}
 		}
