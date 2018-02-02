f9fce8e30877ee90d7261225e759cf6677b52a44
BT_1N3_28 <BT_1N3_28@BT_1N3_28.bktech.local>
2018-02-02 14:31:40 -0500

    w

--- HyperArcadeApplication/src/devin/Inventory.java
+++ HyperArcadeApplication/src/devin/Inventory.java
@@ -2,12 +2,13 @@ package devin;
 
 import java.util.List;
 
+import guiTeacher.components.Button;
 import guiTeacher.components.Graphic;
 import guiTeacher.interfaces.Visible;
 import guiTeacher.userInterfaces.FullFunctionScreen;
 
 public class Inventory extends FullFunctionScreen {
-
+	private Button back;
 	public Inventory(int width, int height) {
 		super(width, height);
 		// TODO Auto-generated constructor stub
@@ -15,8 +16,14 @@ public class Inventory extends FullFunctionScreen {
 
 	@Override
 	public void initAllObjects(List<Visible> viewObjects) {
-		//viewObjects.add(new Graphic(0, 0, getWidth(),getHeight(),"resources/Inventory.jpeg"));
-
+		viewObjects.add(new Graphic(0, 0, getWidth(),getHeight(),"resources/inventory.png"));
+		back = new Button (0,50,200,100,"GO Back",new Action() {
+			public void act() {
+				ArcadeGUI.hyperArcade.setScreen(ArcadeGUI.homeScreen);
+			}
+		});
+		viewObjects.add(back);
+	}
 	}
 
-}
+
