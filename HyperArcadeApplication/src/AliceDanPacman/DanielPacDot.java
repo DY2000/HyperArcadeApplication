fcf1b70d0b6c6f225f11b3b78d3259b8043994c4
BT_1N3_33 <BT_1N3_33@BT_1N3_33.bktech.local>
2018-02-05 15:25:40 -0500

    asdf

--- HyperArcadeApplication/src/AliceDanPacman/DanielPacDot.java
+++ HyperArcadeApplication/src/AliceDanPacman/DanielPacDot.java
@@ -6,3 +6,15 @@ public class DanielPacDot extends DanielPickup {
 		}
 
 	}
+
+/**
+ * They are supposed to appear on the map in an order that is
+ * representative of the pacmans motion. The dots are all same in
+ * size, color, and can only be ate once. If you use a map
+ * '1' would represent the lit dot while '0' would represent an
+ * ate dot. 
+ * 
+ * 
+ * 
+ * 
+ * 
